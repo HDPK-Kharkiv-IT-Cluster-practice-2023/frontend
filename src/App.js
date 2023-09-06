5bde582133a04a5f2479fdf87ebadf66fde0055e
tetyaZyna <sv2408sv@gmail.com>
2023-09-06 21:44:25 +0200

    Create basic form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,116 @@
+import React, { useState } from 'react';
+
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+    const [name, setName] = useState('');
+    const [attack, setAttack] = useState(50);
+    const [armor, setArmor] = useState(50);
+    const [health, setHealth] = useState(50);
+    const [criticalAttack, setCriticalAttack] = useState(50);
+    const [luck, setLuck] = useState(50);
+    const [result, setResult] = useState('');
+
+   /* const handleSubmit = (e) => {
+        e.preventDefault();
+        // Здесь вы можете выполнить логику, основанную на введенных данных
+        // Например, создать бойца с этими характеристиками
+        // и отобразить результат в переменной result
+    };*/
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Создайте объект бойца с характеристиками
+        const fighter = {
+            name,
+            attack,
+            armor,
+            health,
+            criticalAttack,
+            luck,
+        };
+
+        // Здесь можно выполнить дополнительную логику, основанную на характеристиках бойца
+
+        // Отобразите результат
+        const resultText = `Створено бійця:\nІм'я: ${fighter.name}\nАтака: ${fighter.attack}\nЗахист: ${fighter.armor}\nЗдоров'я: ${fighter.health}\nКритичний урон: ${fighter.criticalAttack}\nУдача: ${fighter.luck}`;
+        setResult(resultText);
+    };
+
+    return (
+        <div>
+            <h1>Заповніть форму</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Ім'я:</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                /><br /><br />
+
+                <label htmlFor="attack">Атака:</label>
+                <input
+                    type="range"
+                    id="attack"
+                    name="attack"
+                    min="0"
+                    max="100"
+                    value={attack}
+                    onChange={(e) => setAttack(parseInt(e.target.value))}
+                /><br /><br />
+
+                <label htmlFor="armor">Захист:</label>
+                <input
+                    type="range"
+                    id="armor"
+                    name="armor"
+                    min="0"
+                    max="100"
+                    value={armor}
+                    onChange={(e) => setArmor(parseInt(e.target.value))}
+                /><br /><br />
+
+                <label htmlFor="health">Здоров'я:</label>
+                <input
+                    type="range"
+                    id="health"
+                    name="health"
+                    min="0"
+                    max="100"
+                    value={health}
+                    onChange={(e) => setHealth(parseInt(e.target.value))}
+                /><br /><br />
+
+                <label htmlFor="critical_attack">Критичний урон:</label>
+                <input
+                    type="range"
+                    id="critical_attack"
+                    name="critical_attack"
+                    min="0"
+                    max="100"
+                    value={criticalAttack}
+                    onChange={(e) => setCriticalAttack(parseInt(e.target.value))}
+                /><br /><br />
+
+                <label htmlFor="luck">Удача:</label>
+                <input
+                    type="range"
+                    id="luck"
+                    name="luck"
+                    min="0"
+                    max="100"
+                    value={luck}
+                    onChange={(e) => setLuck(parseInt(e.target.value))}
+                /><br /><br />
+
+                <input type="submit" value="Згенерувати бійця" />
+            </form>
+
+            <div id="result">{result}</div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,36 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("myForm");
+    const resultDiv = document.getElementById("result");
+    const attackSlider = document.getElementById("attack");
+    const armorSlider = document.getElementById("armor");
+    const healthSlider = document.getElementById("health");
+    const critical_attackSlider = document.getElementById("critical_attack");
+    const luckSlider = document.getElementById("luck");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Заполнение формы
+
+        // Получение знач
+        const name = form.elements.name.value;
+        const attack = attackSlider.value;
+        const armor = armorSlider.value;
+        const health = healthSlider.value;
+        const critical_attack = critical_attackSlider.value;
+        const luck = luckSlider.value;
+
+        // Выводод результата
+        resultDiv.innerHTML = "Ім'я:" + name + "<br>" +
+            "Атака:" + attack + "<br>" +
+            "Захист:" + armor + "<br>" +
+            "Здоров'я':" + health + "<br>" +
+            "Критичний урон:" + critical_attack + "<br>" +
+            "Удача:" + luck;
+            });
+            });
